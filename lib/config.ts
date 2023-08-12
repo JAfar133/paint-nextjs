0ad1518d2d8acf5df9a1932be8359d92ac955be3
Misha Golovach <golovach.miha01@gmail.com>
2023-08-12 21:21:45 +0300

    gf

--- lib/config.ts
+++ lib/config.ts
@@ -2,6 +2,6 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 export const BASE_URL = process.env.BASE_URL || 'https://paint-nodejs.vercel.app/'
-    // || 'http://localhost:8000/'
+    || 'http://localhost:8000/'
 export const BASE_SOCKET_URL = process.env.BASE_SOCKET_URL || 'wss://paint-nodejs.vercel.app/'
-    // || 'ws://localhost:8000/'
+    || 'ws://localhost:8000/'
