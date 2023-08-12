599374432916d07040a9835e11a16fd2e9774e3c
Misha Golovach <golovach.miha01@gmail.com>
2023-08-12 20:23:55 +0300

    gf

--- lib/config.ts
+++ lib/config.ts
@@ -1,5 +1,7 @@
 import dotenv from 'dotenv'
 dotenv.config();
 
-export const BASE_URL = process.env.BASE_URL || 'http://localhost:8000/'
-export const BASE_SOCKET_URL = process.env.BASE_SOCKET_URL || 'ws://localhost:8000/'
+export const BASE_URL = process.env.BASE_URL || 'https://paint-nodejs-git-main-jafar133.vercel.app/'
+    // || 'http://localhost:8000/'
+export const BASE_SOCKET_URL = process.env.BASE_SOCKET_URL || 'ws://paint-nodejs-git-main-jafar133.vercel.app/'
+    // || 'ws://localhost:8000/'
