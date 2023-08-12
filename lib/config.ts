15e9699e97bee572c8f447b64e2e87dac99bbf40
Misha Golovach <golovach.miha01@gmail.com>
2023-08-12 21:59:49 +0300

    gf

--- lib/config.ts
+++ lib/config.ts
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv'
 dotenv.config();
 
-export const BASE_URL = process.env.BASE_URL || 'http://212.109.218.146:8000'
-export const BASE_SOCKET_URL = process.env.BASE_SOCKET_URL || 'ws://212.109.218.146:8000'
+export const BASE_URL = 'https://212.109.218.146:8000' || process.env.BASE_URL
+export const BASE_SOCKET_URL = 'wss://212.109.218.146:8000' || process.env.BASE_SOCKET_URL
