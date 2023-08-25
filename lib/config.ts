2fedf4e9d0af34ec6e780ea309112a8b3ffa7d94
Misha Golovach <44347128+JAfar133@users.noreply.github.com>
2023-08-25 11:39:14 +0300

    Delete config.ts

--- lib/config.ts
+++
@@ -1,5 +0,0 @@
-import dotenv from 'dotenv'
-dotenv.config();
-
-export const BASE_URL = process.env.BASE_URL || 'http://localhost:8000/' || 'https://paint-nodejs-3sgr.vercel.app/'
-export const BASE_SOCKET_URL = process.env.BASE_URL || 'ws://localhost:8000/' ||  'wss://paint-nodejs-3sgr.vercel.app/'
