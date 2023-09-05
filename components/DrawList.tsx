c3448e5820456c1bd04e2e02e25825284a0935c8
Misha Golovach <golovach.miha01@gmail.com>
2023-09-05 21:56:39 +0300

    fix drawings page

--- components/DrawList.tsx
+++ components/DrawList.tsx
@@ -12,57 +12,68 @@ interface Image {
 }
 
 const DrawList = () => {
-    const [drawings, setDrawings] = useState<string[]>();
-    const [images, setImages] = useState<Image[]>();
-    const router = useRouter();
+        const [drawings, setDrawings] = useState<string[]>();
+        const [images, setImages] = useState<Image[]>();
+        const router = useRouter();
 
-    useEffect(() => {
-        AuthService.check()
-            .then(response => {
-                const user = response.data.user;
-                userState.setUser({ _id: user._id, username: user.username, email: user.email });
-                userState.setIsAuth(true);
-                UserService.getImages()
-                    .then(res => {
-                        setDrawings(res.data);
-                    });
-            })
-            .finally(() => userState.setLoading(false));
-    }, []);
+        useEffect(() => {
+            AuthService.check()
+                .then(response => {
+                    const user = response.data.user;
+                    userState.setUser({_id: user._id, username: user.username, email: user.email});
+                    userState.setIsAuth(true);
+                    UserService.getImages()
+                        .then(res => {
+                            setDrawings(res.data);
+                        });
+                })
+                .finally(() => userState.setLoading(false));
+        }, []);
 
-    useEffect(() => {
-        if (drawings) {
-            getImages();
+        useEffect(() => {
+            if (drawings) {
+                getImages();
+            }
+        }, [drawings]);
+
+        const getImages = async () => {
+            if (drawings) {
+                const imagesPromises = drawings.map(async (draw) => {
+                    try {
+                        const res = await UserService.getImage(draw);
+                        return {url: draw, src: res.data};
+                    } catch (error) {
+                        console.error(`An error occurred while fetching image for ${draw}:`, error);
+                        return {url: draw, src: null};
+                    }
+                });
+
+                const imagesData = await Promise.all(imagesPromises);
+                const filteredImagesData = imagesData.filter((image) => image.src !== null);
+                setImages(filteredImagesData);
+            }
+        };
+
+
+        const redirectToDraw = (url: string) => {
+            router.push(`/draw/${url}`)
         }
-    }, [drawings]);
 
-    const getImages = async () => {
-        if (drawings) {
-            const imagesPromises = drawings.map(async (draw) => {
-                const res = await UserService.getImage(draw);
-                return { url: draw, src: res.data };
-            });
-
-            const imagesData = await Promise.all(imagesPromises);
-            setImages(imagesData);
-        }
-    };
-
-    const redirectToDraw = (url: string)=>{
-        router.push(`/draw/${url}`)
-    }
-
-    return <div className="w-full pt-20 pl-10 flex flex-wrap justify-center">
-                {images &&
-                    images.map((image, index) => (
-                        <div className="m-5 max-w-md" key={image.url} >
-                            <span>№: {index} url:{image.url}</span>
-                            <div className="border-2 border-primary hover:bg-gray-400 transition duration-300 cursor-pointer" onClick={()=>redirectToDraw(image.url)}>
-                                <NextImage width={500} height={400} src={image.src} alt="" />
-                            </div>
+        return <div className="w-full pt-20 pl-10 flex flex-wrap justify-center">
+            {images &&
+                images.map((image, index) => (
+                    < div className="m-5 max-w-md" key={image.url}>
+                        <span>№: {index} url:{image.url}</span>
+                        <div className="border-2 border-primary hover:bg-gray-400 transition duration-300 cursor-pointer"
+                             onClick={() => redirectToDraw(image.url)}>
+                            <NextImage width={500} height={400} src={image.src} alt=""/>
                         </div>
-                    ))}
-            </div>;
-};
+                    </div>
+                ))
+            }
+        </div>
+            ;
+    }
+;
 
 export default DrawList;
--- lib/api/UserService.ts
+++ lib/api/UserService.ts
@@ -1,4 +1,5 @@
 import {$host, $authHost} from "@/lib/api/index";
+import {log} from "util";
 interface ResponseMessage {
     _id: string;
     username: string;
@@ -7,17 +8,17 @@ interface ResponseMessage {
     canvas_id: string
 }
 export default class UserService {
-    static async saveImage(id: string | string[], dataUrl: string){
+    static saveImage(id: string | string[], dataUrl: string){
         return $host.post(`/image?id=${id}`, {img: dataUrl})
     }
 
-    static async getImage(id: string | string[]){
-        return $host.get(`/image?id=${id}`)
+    static getImage(id: string | string[]){
+        return $host.get(`/image?id=${id}`);
     }
-    static async saveDrawing(id: string | string[]){
+    static saveDrawing(id: string | string[]){
         return $authHost.post(`/drawing?id=${id}`)
     }
-    static async getImages(){
+    static getImages(){
         return $authHost.get('/drawing')
     }
     static async getMessages(canvas_id: string) {
