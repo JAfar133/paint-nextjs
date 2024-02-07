154100ff8c5a8eef245a1527f7d68c47ba6a0cce
Misha Golovach <golovach.miha01@gmail.com>
2024-02-07 20:37:47 +0300

    https trying

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
 const path = require("path");
+const server="https://real-paint.ru/"
 const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination: `${server}/:path*/`,
+      },
+    ]
   },
 }
 
