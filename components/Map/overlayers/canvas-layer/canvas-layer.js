9fe031208d43b5b92257584c7232cf627793cf9d
Vladimir Chernov <vchernov@thomasmurray.com>
2024-09-25 17:24:27 +0300

    optimization

--- components/Map/overlayers/canvas-layer/canvas-layer.js
+++ components/Map/overlayers/canvas-layer/canvas-layer.js
@@ -31,15 +31,17 @@ L.TileLayer.Canvas = L.TileLayer.extend({
     img.onload = () => {
       tile.width = img.width;
       tile.height = img.height;
-      const imageCanvas = this.createImageTile(img);
-      this.drawTile(imageCanvas, coords, tile, done, unwrapCoords);
+      requestAnimationFrame(() => {
+        const imageCanvas = this.createImageTile(img);
+        this.drawTile(imageCanvas, coords, tile, done, unwrapCoords);
+      });
     };
     img.src = isNaN(this._getZoomForUrl()) ? '' : src;
     img.crossOrigin = "Anonymous";
     img.setAttribute('crossOrigin', 'anonymous')
-    
+
     img.role = "presentation";
-    
+
   },
   _delays: {},
   _delaysForZoom: null,
@@ -48,7 +50,7 @@ L.TileLayer.Canvas = L.TileLayer.extend({
     const {x: width, y: height} = this.getTileSize();
     tile.width = doubleSize ? width * 2 : width;
     tile.height = doubleSize ? height * 2 : height;
-    
+
     const img = new Image();
     const tileZoom = this._getZoomForUrl();
     if (tileZoom < 2) MAX_ZOOM = 0
@@ -58,17 +60,18 @@ L.TileLayer.Canvas = L.TileLayer.extend({
     img.onload = () => {
       tile.width = img.width;
       tile.height = img.height;
-      const imageCanvas = this.createImageTile(img)
-      this.drawTile(imageCanvas, coords, tile, done,unwrapCoords)
-      
+      requestAnimationFrame(() => {
+        const imageCanvas = this.createImageTile(img);
+        this.drawTile(imageCanvas, coords, tile, done, unwrapCoords);
+      });
     };
     const src = this.getTileUrl(coords);
-    
+
     img.src = isNaN(tileZoom) ? '' : src;
     img.crossOrigin = "Anonymous";
     img.setAttribute('crossOrigin', 'anonymous')
     img.role = "presentation"
-    
+
   },
   getTileUrl: function (coords) {
     const {x, y} = coords;
@@ -101,7 +104,7 @@ L.TileLayer.Canvas = L.TileLayer.extend({
 
     const canvas = document.createElement('canvas');
     const ctx = canvas.getContext('2d');
-    
+
     canvas.width = img.width;
     canvas.height = img.height;
     ctx.drawImage(img, 0, 0)
@@ -137,9 +140,9 @@ L.TileLayer.Canvas = L.TileLayer.extend({
         if (frozen && frozen > this.frozen_percent && k===5) {
           k = 0;
         }
-        
+
       }
-      
+
     }
   },
   fillAndReverseTile: function (imgData) {
@@ -149,21 +152,21 @@ L.TileLayer.Canvas = L.TileLayer.extend({
     const squareSize = 2;
     const reversedData = new Uint8ClampedArray(data.length);
     let k = 0;
-    
+
     for (let y = 0; y < height; y++) {
       const rowIndex = height - y - 1;
       const random = Math.floor(Math.random() * 32 + 48)
       for (let x = 0; x < width; x++) {
-        
+
         const originalIndex = (y * width + x) * 4;
         const reversedIndex = (rowIndex * width + x) * 4;
-        
+
         const red = data[originalIndex];
         const green = data[originalIndex + 1];
         const blue = data[originalIndex + 2];
-        
+
         const value = this.getValueFromPixel([red, green, blue]);
-        
+
         if (value !== null) {
           let frozen = null;
           if(this.options.data === 'apcp') {
@@ -233,12 +236,12 @@ L.TileLayer.Canvas = L.TileLayer.extend({
         tile.width,
         tile.height);
     }
-    
-    
+
+
     const imgData = ctx.getImageData(0, 0, tile.width, tile.height)
-    
+
     const tileEl = this._tiles[this._tileCoordsToKey(unwrapCoords)]
-    
+
     if (tileEl) {
       tileEl.imgData = imgData;
     }
@@ -246,7 +249,7 @@ L.TileLayer.Canvas = L.TileLayer.extend({
     const tileCtx = tile.getContext('2d')
     tileCtx.imageSmoothingEnabled = 'false'
     tileCtx.putImageData(reversedImgData, 0, 0)
-    
+
     tile.complete = true;
     done(null, tile);
   },
@@ -274,7 +277,7 @@ L.TileLayer.Canvas = L.TileLayer.extend({
         lowerIndex = i;
       }
     }
-    
+
     if (lowerIndex === upperIndex) {
       if (upperIndex !== gradient.length - 1)
         upperIndex++;
@@ -282,16 +285,16 @@ L.TileLayer.Canvas = L.TileLayer.extend({
     }
     const lowerValue = gradient[lowerIndex].value;
     const upperValue = gradient[upperIndex].value;
-    
+
     const fraction = (value - lowerValue) / (upperValue - lowerValue);
-    
+
     return [
       Math.max(0, Math.round(gradient[lowerIndex].data[0] + fraction * (gradient[upperIndex].data[0] - gradient[lowerIndex].data[0]))),
       Math.max(0, Math.round(gradient[lowerIndex].data[1] + fraction * (gradient[upperIndex].data[1] - gradient[lowerIndex].data[1]))),
       Math.max(0, Math.round(gradient[lowerIndex].data[2] + fraction * (gradient[upperIndex].data[2] - gradient[lowerIndex].data[2]))),
     ];
   },
-  
+
   getValueFromPixel: function (pixel) {
     const [r, g, b] = pixel;
     let data = null;
@@ -330,44 +333,44 @@ L.TileLayer.Canvas = L.TileLayer.extend({
     const {timeout} = this.options;
     const {z: zoom} = coords;
     const tile = document.createElement("canvas");
-    
+
     if (timeout) {
       if (zoom !== this._delaysForZoom) {
         this._clearDelaysForZoom();
         this._delaysForZoom = zoom;
       }
-      
+
       if (!this._delays[zoom]) this._delays[zoom] = [];
-      
+
       this._delays[zoom].push(setTimeout(() => {
         this.createCanvas(tile, coords, done, unwrapCoords);
       }, timeout));
     } else {
       this.createCanvas(tile, coords, done, unwrapCoords);
     }
-    
+
     return tile;
   },
   _addTile: function (coords, container) {
     const tilePos = this._getTilePos(coords),
       key = this._tileCoordsToKey(coords);
-    
+
     const tile = this.createTile(this._wrapCoords(coords), bind(this._tileReady, this, coords), coords);
     this._initTile(tile);
-    
+
     if (this.createTile.length < 2) {
       requestAnimFrame(bind(this._tileReady, this, coords, null, tile));
     }
-    
+
     setPosition(tile, tilePos);
-    
+
     // save tile in cache
     this._tiles[key] = {
       el: tile,
       coords: coords,
       current: true
     };
-    
+
     container.appendChild(tile);
     this.fire('tileloadstart', {
       tile: tile,
@@ -377,17 +380,17 @@ L.TileLayer.Canvas = L.TileLayer.extend({
   _clearDelaysForZoom: function () {
     const prevZoom = this._delaysForZoom;
     const delays = this._delays[prevZoom];
-    
+
     if (!delays) return;
-    
+
     delays.forEach((delay, index) => {
       clearTimeout(delay);
       delete delays[index];
     });
-    
+
     delete this._delays[prevZoom];
   },
-  
+
 });
 
 L.tileLayer.canvas = function tileLayerCanvas(url, options) {
