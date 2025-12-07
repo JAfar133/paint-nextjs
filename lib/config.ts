8d2152f0a3c61b0bcaec0778f7d73dd10c23db86
Sergey Kurepin <skurepin@thomasmurray.com>
2025-12-07 15:24:28 +0300

    Fix deploy

--- lib/config.ts
+++ lib/config.ts
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv'
 dotenv.config();
 
-export const BASE_URL = process.env.BASE_URL || 'http://localhost:5000/'
-export const BASE_SOCKET_URL = process.env.BASE_URL || 'ws://localhost:5000/'
+export const BASE_URL = process.env.BASE_URL || 'http://localhost:5045/'
+export const BASE_SOCKET_URL = process.env.BASE_URL || 'ws://localhost:5045/'
