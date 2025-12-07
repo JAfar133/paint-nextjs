de647f5ea2c1f5ffc2af8b9bccc811d758fe7eee
Sergey Kurepin <skurepin@thomasmurray.com>
2025-12-07 22:32:09 +0300

    fix environment

--- lib/config.ts
+++ lib/config.ts
@@ -1,5 +1,8 @@
 import dotenv from 'dotenv'
 dotenv.config();
 
-export const BASE_URL = process.env.BACKEND_HTTP_URL || 'http://localhost:5045/'
-export const BASE_SOCKET_URL = process.env.BACKEND_WS_URL || 'ws://localhost:5045/'
+export const BASE_URL =
+    process.env.NEXT_PUBLIC_BACKEND_HTTP_URL || 'http://localhost:8000/'
+
+export const BASE_SOCKET_URL =
+    process.env.NEXT_PUBLIC_BACKEND_WS_URL || 'ws://localhost:8000/'
