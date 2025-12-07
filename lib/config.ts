8d56ca11811527c9fe5976cfa53bafda2170d9dd
Sergey Kurepin <skurepin@thomasmurray.com>
2025-12-07 17:03:00 +0300

    Fix bugs

--- lib/config.ts
+++ lib/config.ts
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv'
 dotenv.config();
 
-export const BASE_URL = process.env.BASE_URL || 'http://localhost:5045/'
-export const BASE_SOCKET_URL = process.env.BASE_URL || 'ws://localhost:5045/'
+export const BASE_URL = process.env.BACKEND_HTTP_URL || 'http://localhost:5045/'
+export const BASE_SOCKET_URL = process.env.BACKEND_WS_URL || 'ws://localhost:5045/'
